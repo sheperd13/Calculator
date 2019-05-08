b80a1f0c6f4d8e7d82c9f47f47b6c428f1de1710
sheperd13 <johncleishman@gmail.com>
2019-05-08 00:13:55 -0600

    new calc

---
+++ Calculator/CalculatorViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class CalculatorViewModel
+    {
+        public double DisplayValue { get; set; }
+
+        public CalculatorViewModel()
+        {
+            DisplayValue = 0.0123;
+        }
+    }
+}
